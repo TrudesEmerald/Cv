684fa93e95a53da69cbc4ba58e43f80593736c2b
TrudesEmerald <getrudekyeikbh@gmail.com>
2022-04-19 18:55:01 +0000

    Cv

---
+++ src/CvApp.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+function CvApp( ){
+    return (
+        <div className='App'>
+        <h2>GETRUDE KYEI BAFFOUR</h2>
+        <h4>CURRICULUM VITAE</h4>
+        <p>techytrudy@gmail.com</p>
+        <p>I am Getrude Kyei Baffour from Ghana. I am 21 years of age. I live in Ghana.
+     
+<h3>EDUCATION AND SKILLS</h3>
+<p>I graduated from Codeafric web development bootcamp. I am passionate about  <br/> coding and solving problems through code.
+I have expertise in the following skills:</p>
+
+
+<ul>
+<li>HTML</li>
+
+<li>CSS </li>
+
+<li>Javascript</li>
+
+<li>Bootstrap</li>
+
+<li>Git </li>
+
+<li>Wordpress, Wordpress plugins </li>
+
+<li>Microsoft Office suite </li>
+</ul>
+ </p>
+
+<h3>WORK EXPERIENCE</h3>
+<h4>Wordpress Developer</h4>
+<p>I developed a wholesale e-commerce company where vendors can sign up and sell on the website at wholesale prices.
+
+ </p>
+
+<h4>Intern </h4> Oct 2020 - Dec 2020
+<p>Two months Graphic Design internship with Nakon media.
+ </p>
+          
+           </div>
+           
+    );
+}
+
+
+export default CvApp;
